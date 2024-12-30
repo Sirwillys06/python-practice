b4708265e5fb16d0bcd1b1a517985a544b967f5c
Sir Willys <whurtadob@tecnocomfenalco.edu.co>
2024-12-30 15:11:49 -0500

    2Commit

---
+++ conversion_datos.py
@@ -0,0 +1,38 @@
+
+#Para convertir un dato a enterio se utiliza la función int()
+#Para convertir un dato a flotante se utiliza la función float()
+#Para convertir un dato a cadena se utiliza la función str()
+#Para convertir un dato a booleano se utiliza la función bool()
+
+
+#Conversion de datos
+
+
+#convertir de cadena a numero entero:
+
+cadena= "1234"
+numero= int(cadena) #Convierte la cadena a numero entero
+print(f"El numero convertido a int es: {numero}")
+
+
+cadena2= "12.34"
+numero2= float(cadena2) #Convierte la cadena a numero flotante
+print(f"El numero convertido a float es: {numero2}")
+
+
+cadena3= "1234"
+numero3= str(cadena3) #Convierte el numero a cadena
+print(f"El numero convertido a str es: {numero3}")
+
+
+numero= 25
+conversion= str(numero) #Convierte el numero a cadena
+print(f"El numero convertido a str es: {conversion}")
+
+
+cadena= "True"
+booleano= bool(cadena) #Convierte la cadena a booleano
+print(f"El booleano convertido a bool es: {booleano}")
+
+
+
